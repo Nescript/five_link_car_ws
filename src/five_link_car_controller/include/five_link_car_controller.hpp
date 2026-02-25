2ec098068b09d7c615437dfe227d6a605b325248
Nescript <raymond059@qq.com>
2026-02-25 20:23:27 +0800

    Update script names

--- src/five_link_car_controller/include/five_link_car_controller.hpp
+++
@@ -1,4 +0,0 @@
-// This file is kept for backward compatibility.
-// The actual header is at five_link_car_controller/five_link_car_controller.hpp
-#pragma once
-#include "five_link_car_controller/five_link_car_controller.hpp"
